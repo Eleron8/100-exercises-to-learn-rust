20af5b133888e3e6313dfb088c9584b14c71923e
eleron8 <eleron888@gmail.com>
2025-10-10 05:42:57 +0100

    3rd run

--- exercises/04_traits/01_trait/src/lib.rs
+++ exercises/04_traits/01_trait/src/lib.rs
@@ -3,9 +3,29 @@
 //
 // Then implement the trait for `u32` and `i32`.
 
+
+trait IsEven {
+    fn is_even(&self) -> bool;
+}
+
+
+impl IsEven for u32 {
+    fn is_even(&self) -> bool {
+        self % 2 == 0
+    }
+}
+
+impl IsEven for i32 {
+    fn is_even(&self) -> bool {
+        self %2 == 0
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
-    use super::*;
+    // use super::*;
+    use crate::IsEven;
 
     #[test]
     fn test_u32_is_even() {
