3dc680c2ad20fa19015e7502681547f8e7c81686
Almas Maratov <amaratov@quantumbrains.com>
2025-10-13 23:30:10 +0400

    another part of 6

--- exercises/06_ticket_management/11_mutable_slices/src/lib.rs
+++ exercises/06_ticket_management/11_mutable_slices/src/lib.rs
@@ -1,6 +1,12 @@
 // TODO: Define a function named `squared` that raises all `i32`s within a slice to the power of 2.
 //  The slice should be modified in place.
 
+fn squared(sl: &mut [i32]) {
+    for el in sl.iter_mut() {
+        *el *= *el
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
